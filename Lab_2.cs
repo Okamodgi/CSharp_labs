0fdcb189e9539e7b2dfb0e562acfa6e10892f468
Okamodgi <123985263+Okamodgi@users.noreply.github.com>
2024-04-06 11:30:21 +0300

    Add files via upload

---
+++ Lab_2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+class Lab_2
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите номер дня недели: \n" +
+            "Понедельник = 1\r\n" +
+            "Вторник = 2\r\n" +
+            "Среда = 3\r\n" +
+            "Четверг = 4\r\n" +
+            "Пятница = 5\r\n" +
+            "Суббота = 6\r\n" +
+            "Воскресенье = 7 ");
+        int week = Convert.ToInt32(Console.ReadLine());
+        int valWeek = DayOfWeek(week);
+        if (valWeek == -1)
+            return;
+
+        Console.WriteLine("Введите сколько часов: ");
+        int timeHour = Convert.ToInt32(Console.ReadLine());
+        int valHour = Times(timeHour);
+        if (valHour == -1)
+            return;
+
+        Console.WriteLine(" ");
+
+        int timesHour = Times(timeHour);
+        
+        string weeks = Enum.GetName(typeof(DayOfTheWeek), week);
+        string timeOfDay = Enum.GetName(typeof(TimeDay), timesHour);
+
+        Console.WriteLine($"Сейчас {weeks}, {timeOfDay}" );
+
+    }
+    public static int Times(int time)
+    {
+        if (time >= 7 && time <= 12)
+        {
+            return (int)TimeDay.Утро;
+        }
+        else if (time >= 13 && time <= 18)
+        {
+            return (int)TimeDay.День;
+        }
+        else if (time >= 19 && time <= 23)
+        {
+            return (int)TimeDay.Вечер;
+        }
+        else if (time >= 0 && time <= 6) 
+        {
+            return (int)TimeDay.Ночь;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: некорректное значение времени.");
+            return -1;
+        }
+    }
+
+    public static int DayOfWeek(int day)
+    {
+        if (day >= 1 && day <= 7)
+        {
+            return day;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: некорректный номер дня недели.");
+            return -1;
+        }
+    }
+
+    enum DayOfTheWeek : int
+    {
+        Понедельник = 1,
+        Вторник,
+        Среда,
+        Четверг,
+        Пятница,
+        Суббота,
+        Воскресенье
+    }
+
+    enum TimeDay : int 
+    {
+        Утро,
+        День,
+        Вечер,
+        Ночь
+    }
+}
+
