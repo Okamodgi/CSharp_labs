78a11b4f9aad7856fe7b64bd252fc0c938ea9ef3
Okamodgi <123985263+Okamodgi@users.noreply.github.com>
2024-04-06 10:09:32 +0300

    Laboratornaya_1

---
+++ Lab_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите N");
+        int N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите X");
+        int x = Convert.ToInt32(Console.ReadLine());
+        double res = 0;
+
+        for (int n = 0; n <= N; n++)
+        {
+            res += (Math.Pow(-1, n) * (Math.Pow(x, 2 * n)) / Factorial(n));
+
+            Console.WriteLine(Factorial(n));
+        }
+        Console.WriteLine(res);
+    }
+
+    public static double Factorial(int n)
+    {
+        double result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+
+    }
+}
